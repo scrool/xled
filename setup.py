5977dd16a0ce8503887328a80316f9bd584d5438
Pavol Babinčák <scroolik@gmail.com>
2018-01-02 21:04:23 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name='xled',
-    version='0.2.0',
+    version='0.2.1',
     description=("Python library and unofficial API documentation for Twinkly "
                  "- Smart Decoration LED lights for Christmas."),
     long_description=readme + '\n\n' + history,
