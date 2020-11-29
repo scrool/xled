037d0517cb9cc04012b7070c4343de7e1298c748
Shane Andrusiak <shane.andrusiak@wildtv.ca>
2020-11-29 12:25:14 -0700

    Change setup to get rid of arpreq and swap in getmac

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "arpreq",
+    "getmac",
     "click-log",
     "Click>=6.0",
     "cryptography",
