b823c2d62f3656905b938e656091bbec8ad5eae3
Pavol Babinčák <scroolik@gmail.com>
2022-10-31 19:24:24 +0100

    Add 3.11 to supported Python versions

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     project_urls={
         "Documentation": "https://xled.readthedocs.io/",
